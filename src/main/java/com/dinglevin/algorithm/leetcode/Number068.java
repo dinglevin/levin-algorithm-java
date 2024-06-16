ec30db21af7da2446ee804378a15445a6a5664f7
dinglevin <dingweiqiong@gmail.com>
2024-06-16 18:28:09 +0800

    Number001添加测试用例

--- src/main/java/com/dinglevin/algorithm/leetcode/Number068.java
+++ src/main/java/com/dinglevin/algorithm/leetcode/Number068.java
@@ -8,7 +8,8 @@ import java.util.List;
  * 问题描述:
  * 文本左右对齐
  * 1、给定一个单词数组 words 和一个长度 maxWidth ，重新排版单词，使其成为每行恰好有 maxWidth 个字符，且左右两端对齐的文本。
- * 2、你应该使用 “贪心算法” 来放置给定的单词；也就是说，尽可能多地往每行中放置单词。必要时可用空格 ' ' 填充，使得每行恰好有 maxWidth 个字符。
+ * 2、你应该使用 “贪心算法” 来放置给定的单词；也就是说，尽可能多地往每行中放置单词。必要时可用空格 ' ' 填充，使得每行恰好有 maxWidth
+ * 个字符。
  * 3、要求尽可能均匀分配单词间的空格数量。如果某一行单词间的空格不能均匀分配，则左侧放置的空格数要多于右侧的空格数。
  * 4、文本的最后一行应为左对齐，且单词之间不插入额外的空格。
  *
@@ -22,7 +23,7 @@ import java.util.List;
  */
 public class Number068 {
     public static void main(String[] args) {
-        String[] words = new String[] {"This", "is", "an", "example", "of", "text", "justification."};
+        String[] words = new String[] { "This", "is", "an", "example", "of", "text", "justification." };
         int maxWidth = 16;
         System.out.println(fullJustify(words, maxWidth));
     }
